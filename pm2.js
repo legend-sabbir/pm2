b3217109ab53802d1145e2b552aae58f5dc94e61
Sabbir <sabbirhossain.trash@gmail.com>
2023-05-27 13:48:30 +0000

    pm2

---
+++ pm2.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const sleep = (ms) =>  new Promise(resolve => setTimeout(resolve, ms))
+const arr = []
+
+app.get('/start', (req, res) => {
+  res.send("started")
+  for (let i = 0; i < 50; i++) {
+    arr.push(i)
+  }
+});
+
+let track = 1
+
+app.get('/get', (req, res) => {
+  res.json(arr)
+  console.log("success: " + track++)
+});
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
